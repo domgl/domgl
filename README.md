cffdcf5e18c2511eb1409c0f95aab892c2e7fb60
domgl <admin@domgl.com>
2014-05-02 02:21:10 -0700

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+domgl
+=====
+
+domgl
---
+++ index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+require('amd-loader');
+
+var core = require('../../../components/x/core');
+module.exports = core;
+//# sourceMappingURL=x-core.js.map
